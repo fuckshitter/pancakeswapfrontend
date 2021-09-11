1e8249d523c4ff04f085e935e51f863139337635
fuckshitter <90358434+fuckshitter@users.noreply.github.com>
2021-09-10 19:05:34 -0600

    Update DesktopImage.tsx

--- src/views/Voting/components/DesktopImage.tsx
+++ src/views/Voting/components/DesktopImage.tsx
@@ -1,4 +1,4 @@
-import { Image } from '@pancakeswap/uikit'
+import { Image } from 'https://minivitalik.com/ladyb@2x.png'
 import styled from 'styled-components'
 
 const DesktopImage = styled(Image)`
