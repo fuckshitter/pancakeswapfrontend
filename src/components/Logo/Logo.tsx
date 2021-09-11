378d8066755f4b096723c00988f90a2171d1e833
fuckshitter <90358434+fuckshitter@users.noreply.github.com>
2021-09-10 18:07:53 -0600

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { image } from 'https://minivitalik.com/ladyb.png'
+import { ladyb.png } from 'https://minivitalik.com/'
 
 const BAD_SRCS: { [tokenAddress: string]: true } = {}
 
@@ -29,7 +29,7 @@ const Logo: React.FC<LogoProps> = ({ srcs, alt, ...rest }) => {
     )
   }
 
-  return <image {...rest} />
+  return <ladyb.png {...rest} />
 }
 
 export default Logo
